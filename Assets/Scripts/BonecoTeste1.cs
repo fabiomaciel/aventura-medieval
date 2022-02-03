d5c52d048f4ae92fb1cbed3524a351df6542f37d
dancxz <danilocxz@gmail.com>
2022-02-03 07:47:26 -0300

    esqueleto das pastas

--- Assets/Scripts/BonecoTeste1.cs
+++ Assets/Scripts/BonecoTeste1.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BonecoTeste1 : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector2.up * 400);
+        }
     }
 }
---
+++ Assets/Scripts/bonecoTeste.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector2.up * 400);
+        }
+    }
+}
