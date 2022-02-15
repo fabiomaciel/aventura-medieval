1b28c137b84977958550ad396ffceece18fc73fd
fabio maciel <6810827+fabiomaciel@users.noreply.github.com>
2022-02-15 20:23:28 -0300

    feat: add camera movment

---
+++ Assets/CameraController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;        //Public variable to store a reference to the player game object
+
+
+    private Vector3 offset;            //Private variable to store the offset distance between the player and camera
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
