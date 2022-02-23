a02eabad4607a317c7a4849e6c49567b45021b94
dancxz <danilocxz@gmail.com>
2022-02-22 21:35:19 -0300

    correcoes

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;        //Public variable to store a reference to the player game object
+    GameObject _Player;     //Public variable to store a reference to the player game object
 
 
-    private Vector3 offset;            //Private variable to store the offset distance between the player and camera
+    //private Vector2 offset;            //Private variable to store the offset distance between the player and camera
     
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.transform.position;
-        
+        _Player = GameObject.FindGameObjectWithTag("Player");
+        //offset = player.transform.position;//transform.position - player.transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
+
     {
-        transform.position = player.transform.position + offset;
+        if (_Player == null)
+        {
+            _Player = GameObject.FindGameObjectWithTag("Player");
+        }
+        else
+        {
+            float PlayerX = _Player.transform.position.x;
+            float PlayerY = _Player.transform.position.y;
+            transform.position = new Vector3(PlayerX, PlayerY + 1.8f, -10);
+            Debug.Log(_Player.transform.position);
+        }
+        //offset = player.transform.position;//transform.position - player.transform.position;
+        //transform.position = offset;//player.transform.position + offset;
     }
 }
